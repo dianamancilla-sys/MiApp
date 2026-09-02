683b340835d5d820fd02652ca1242ad95360b7e0
Diana <diana@gmail.com>
2026-09-01 22:37:14 -0600

    Modulo 4 Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
+import { firebase } from "@nativescript/firebase";
 import { NativeScriptModule } from '@nativescript/angular'
 import { NativeScriptUISideDrawerModule } from 'nativescript-ui-sidedrawer/angular'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-
 @NgModule({
 
   imports: [AppComponent, AppRoutingModule, NativeScriptModule, NativeScriptUISideDrawerModule],
 
   schemas: [NO_ERRORS_SCHEMA],
 })
-export class AppModule {}
+export class AppModule { 
+  constructor() {
+    firebase.init({
+      onMessageReceivedCallback: (message) => {
+        console.log("Firebase Token:", message);
+      }
+    });
+  }
+}
